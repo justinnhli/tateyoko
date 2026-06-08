d43a797099c91ef8d5c70e10204a4fb8b0f28e24
Justin Li <justinnhli@gmail.com>
2026-06-08 09:26:44 -0700

    mark regions within OCR bboxes as characters

--- tateyoko.py
+++ tateyoko.py
@@ -721,6 +721,28 @@ def pipeline(path, args):
         ocr_mask[ocr_min_row:ocr_max_row, ocr_min_col] = 1
         ocr_mask[ocr_min_row:ocr_max_row, ocr_max_col] = 1
     check_time('loaded OCR results')
+    # mark regions within bounding boxes as characters (FIXME unoptimized)
+    min_dimension = min(array.shape[0], array.shape[1]) // 100
+    max_dimension = min(array.shape[0], array.shape[1]) // 4
+    for ocr_id, (ocr_min_col, ocr_min_row, ocr_max_col, ocr_max_row) in ocr_bboxes.items():
+        if ocr_max_col == ocr_mask.shape[1]:
+            ocr_max_col -= 1
+        if ocr_max_row == ocr_mask.shape[0]:
+            ocr_max_row -= 1
+        temp_mask = np.zeros(char_mask.shape).astype(np.bool)
+        temp_mask[ocr_min_row:ocr_max_row, ocr_min_col:ocr_max_col] = 1
+        temp_mask = temp_mask & misc_mask
+        labels = skimage_label(temp_mask)
+        for region in regionprops(labels):
+            if region_is_character(region, min_dimension, max_dimension):
+                char_mask = char_mask | temp_mask
+                misc_mask = (misc_mask ^ temp_mask) ^ temp_mask
+    visualize(
+        (misc_mask, (255, 255, 255)),
+        (char_mask, (0, 255, 0)),
+        (ocr_mask, (0, 0, 255)),
+    )
+    check_time('adjusted characters based on OCR')
     # associate characters with OCR regions (FIXME unoptimized)
     char_to_ocr = defaultdict(set)
     ocr_to_char = defaultdict(set)
@@ -743,27 +765,6 @@ def pipeline(path, args):
             if intersects:
                 char_to_ocr[region_id].add(ocr_id)
                 ocr_to_char[ocr_id].add(region_id)
-    visualize(
-        (misc_mask, (255, 255, 255)),
-        (char_mask, (0, 255, 0)),
-        (ocr_mask, (0, 0, 255)),
-    )
-    # mark other pixels within bounding boxes as characters
-    min_dimension = min(array.shape[0], array.shape[1]) // 100
-    max_dimension = min(array.shape[0], array.shape[1]) // 4
-    for ocr_id, (ocr_min_col, ocr_min_row, ocr_max_col, ocr_max_row) in ocr_bboxes.items():
-        if ocr_max_col == ocr_mask.shape[1]:
-            ocr_max_col -= 1
-        if ocr_max_row == ocr_mask.shape[0]:
-            ocr_max_row -= 1
-        temp_mask = np.zeros(char_mask.shape).astype(np.uint8)
-        temp_mask[ocr_min_row:ocr_max_col, ocr_min_col:ocr_max_col] = 1
-        temp_mask = temp_mask & misc_mask
-        labels = skimage_label(temp_mask)
-        for region in regionprops(labels):
-            if region_is_character(region, min_dimension, max_dimension):
-                char_mask = char_mask | temp_mask
-                misc_mask = misc_mask - temp_mask
     visualize(
         (misc_mask, (255, 255, 255)),
         (char_mask, (0, 255, 0)),
